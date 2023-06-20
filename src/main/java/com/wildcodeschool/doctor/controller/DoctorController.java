289c702df0f757ec57dd1293f3cacd127f29cfa7
msykhanou <msykhanou@yahoo.fr>
2023-06-20 09:37:57 +0200

    first commit

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,12 +1,21 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class DoctorController {
+
+
+    List<Doctor> doctors = new ArrayList<Doctor>();
 
     @GetMapping("/doctor/")
     @ResponseBody
@@ -14,4 +23,21 @@ public class DoctorController {
 
         return null;
     }
+
+    @GetMapping("/doctor/{id}")
+    @ResponseBody
+    Doctor getDoctorById(@PathVariable int id) {
+
+       if (id==13){
+           return new Doctor(13,"Jodie Whittaker");
+       }
+       else if(id >=1 && id <= 12)
+       {
+           throw new ResponseStatusException(HttpStatus.SEE_OTHER, "See Other");
+       } else {
+           throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + id + " .");
+       }
+    }
+
+
 }
